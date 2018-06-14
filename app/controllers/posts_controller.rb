f20c187eb3339eac8579dff58a1df9d6dca4ea88
fuchida64 <crbsfy@icloud.com>
2018-06-15 03:14:56 +0900

    m2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,22 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
+  def create
+  	post = Post.new(post_params)
+  	post.save
+  	redirect_to post_path(post.id)
+  end
+
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  	def post_params
+  		params.require(:post).permit(:title, :body)
+  	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/' =>'posts#top'
   get '/posts' => 'posts#index'
+  get '/posts/:id' => 'posts#show', as: 'post'
   resources :posts
 end
