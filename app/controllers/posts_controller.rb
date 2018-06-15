6d01ec1df90e76d8f61da7d3eeabb0b4b6f9a21e
fuchida64 <crbsfy@icloud.com>
2018-06-15 22:32:00 +0900

    m6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,41 +3,51 @@ class PostsController < ApplicationController
   end
 
   def index
-  	@posts = Post.all
-  	@post = Post.new
+    @posts = Post.all
+    @post = Post.new
   end
 
   def create
     @posts = Post.all
-  	@post = Post.new(post_params)
-  	if @post.save
-  	 redirect_to post_path(@post.id)
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to post_path(@post.id)
     else
       render 'index'
     end
   end
 
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def edit
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def update
-  	post = Post.find(params[:id])
-  	post.update(post_params)
-  	redirect_to post_path(post)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to post_path(@post.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
-    redirect_to posts_path
-	end
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "Book was successfully created."
+      redirect_to posts_path
+    else
+      render 'index'
+    end
+  end
+
   private
-  	def post_params
-  		params.require(:post).permit(:title, :body)
-  	end
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
